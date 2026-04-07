30c3045155a1c057171506aaf3ffee64552df679
asivery <asivery@protonmail.com>
2026-04-07 04:04:05 +0200

    Start working on version 1.5.4

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ serviceRegistry.mediaRecorderService = new MediaRecorderService();
 serviceRegistry.mediaSessionService = new BrowserMediaSessionService(store);
 
 Object.defineProperty(window, 'wmdVersion', {
-    value: '1.5.3',
+    value: '1.5.4',
     writable: false,
 });
 
